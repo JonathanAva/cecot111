63e93cd5ff856efeabafadcc2a775e2ddd7f81e0
JonathanAva <hpalvarez10@gmail.com>
2024-08-20 22:51:01 -0600

    dashboard user

--- cecot_system/resources/views/admin/dashboard.blade.php
+++ cecot_system/resources/views/admin/dashboard.blade.php
@@ -54,34 +54,49 @@
             </div>
 
            
-            <div class="col-lg-6 col-md-6 col-sm-12 mb-4 mt-5">
-                <a href="#" class="card mb-3 text-decoration-none" style="height: 250px;">
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-4">
+                <a href="#" class="card mb-3 text-decoration-none" style="height: 200px;">
                     <div class="row g-0 h-100">
-                        <div class="col-md-4 bg-custom-large d-flex align-items-center justify-content-center">
-                            <img src="{{ asset('images/generar_reportes.png') }}" class="img-fluid icon-large" alt="Generar Reportes">
+                        <div class="col-md-4 bg-custom d-flex align-items-center justify-content-center">
+                            <img src="{{ asset('images/generar_reportes.png') }}" class="img-fluid icon" alt="Gestionar Visitas">
                         </div>
                         <div class="col-md-8 d-flex align-items-center">
                             <div class="card-body text-dark">
-                                <h5 class="card-title">Generar Reportes</h5>
-                                <p class="card-text">Crea y visualiza reportes detallados.</p>
+                                <h5 class="card-title">GENERA REPORTES</h5>
+                                <p class="card-text">Administra y organiza los reportes.</p>
                             </div>
                         </div>
                     </div>
                 </a>
 
-                <a href="#" class="card mb-3 text-decoration-none" style="height: 250px;">
+                <a href="#" class="card mb-3 text-decoration-none" style="height: 200px;">
                     <div class="row g-0 h-100">
-                        <div class="col-md-4 bg-custom-large d-flex align-items-center justify-content-center">
-                            <img src="{{ asset('images/ver_planillas.png') }}" class="img-fluid icon-large" alt="Ver Planillas">
+                        <div class="col-md-4 bg-custom d-flex align-items-center justify-content-center">
+                            <img src="{{ asset('images/ver_planillas.png') }}" class="img-fluid icon" alt="Gestionar Presos">
                         </div>
                         <div class="col-md-8 d-flex align-items-center">
                             <div class="card-body text-dark">
-                                <h5 class="card-title">Ver Planillas</h5>
-                                <p class="card-text">Consulta las planillas actualizadas.</p>
+                                <h5 class="card-title">VER PLANILLA</h5>
+                                <p class="card-text">Gestión la planilla de CECOT.</p>
                             </div>
                         </div>
                     </div>
                 </a>
+
+                <a href="#" class="card mb-3 text-decoration-none" style="height: 200px;">
+                    <div class="row g-0 h-100">
+                        <div class="col-md-4 bg-custom d-flex align-items-center justify-content-center">
+                            <img src="{{ asset('images/gestionarEmpleado.png') }}" class="img-fluid icon" alt="Gestionar Celdas">
+                        </div>
+                        <div class="col-md-8 d-flex align-items-center">
+                            <div class="card-body text-dark">
+                                <h5 class="card-title">GESTIONAR EMPLEADOS</h5>
+                                <p class="card-text">Administra y organiza los empleados.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                
             </div>
         </div>
     </div>
--- cecot_system/resources/views/user/dashboard.blade.php
+++ cecot_system/resources/views/user/dashboard.blade.php
@@ -1,24 +1,52 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard del Usuario</title>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Usuario</span>
-            <form action="{{ route('logout') }}" method="POST" class="d-flex">
-                @csrf
-                <button type="submit" class="btn btn-outline-light">Cerrar sesión</button>
-            </form>
-        </div>
-    </nav>
+@extends('layouts.app')
+
+@section('content')
+    @include('partials.navbar_user')
 
     <div class="container mt-4">
-        <h1>Bienvenido, Usuario {{ Auth::user()->nombre_usuario }}</h1>
+        <h1 class="text-center">Bienvenido al Sistema de Gestión Penitenciario</h1>
+
+        <div class="row mt-5">
+            
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-4">
+                <a href="#" class="card mb-3 text-decoration-none" style="height: 200px;">
+                    <div class="row g-0 h-100">
+                        <div class="col-md-4 bg-custom d-flex align-items-center justify-content-center">
+                            <img src="{{ asset('images/gestionar_visitas.png') }}" class="img-fluid icon" alt="Gestionar Visitas">
+                        </div>
+                        <div class="col-md-8 d-flex align-items-center">
+                            <div class="card-body text-dark">
+                                <h5 class="card-title">Gestionar Visitas</h5>
+                                <p class="card-text">Administra y organiza las visitas a los internos.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                
+                
+            </div>
+
+           
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-4">
+                <a href="#" class="card mb-3 text-decoration-none" style="height: 200px;">
+                    <div class="row g-0 h-100">
+                        <div class="col-md-4 bg-custom d-flex align-items-center justify-content-center">
+                            <img src="{{ asset('images/ver_planillas.png') }}" class="img-fluid icon" alt="Gestionar Presos">
+                        </div>
+                        <div class="col-md-8 d-flex align-items-center">
+                            <div class="card-body text-dark">
+                                <h5 class="card-title">VER PLANILLA</h5>
+                                <p class="card-text">Gestión la planilla de CECOT.</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+               
+
+                
+            </div>
+        </div>
     </div>
-</body>
-</html>
+@endsection
